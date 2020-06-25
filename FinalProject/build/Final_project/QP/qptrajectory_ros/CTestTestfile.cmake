bd42601213e7389b27f795fbaa59459a7c09ae04
Jiunwei97 <jiunwei97@gamil.com>
2020-06-25 15:33:06 +0800

    homeworks uploaded

--- FinalProject/build/Final_project/QP/qptrajectory_ros/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ncrl/gener_ws/src/Final_project/QP/qptrajectory_ros
-# Build directory: /home/ncrl/gener_ws/build/Final_project/QP/qptrajectory_ros
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- FinalProject/build/offboard/CMakeFiles/motive.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for motive.
-# This may be replaced when dependencies are built.
